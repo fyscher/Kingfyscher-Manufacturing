f5451a58e2f10f6dda34dd12fe004d7343b12fb0
Fyscher Kraal <fyscher.kraal@gmail.com>
2025-07-06 12:13:37 -0600

    add login functionality

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 const middleware = require('./middleware')
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('body', req => JSON.stringify(req.body))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(middleware.tokenExtractor)
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
--- utils/config.js
+++ utils/config.js
@@ -6,8 +6,15 @@ const MONGODB_URI = process.env.NODE_ENV === 'test'
     ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI
 
+const DEVSHOPID = process.env.DEVSHOPID
+const KFM_PASSWORD = process.env.KFM_PASSWORD
+const UPLAND_URI = process.env.UPLAND_URI
+
 module.exports = 
 {
     PORT,
     MONGODB_URI,
+    DEVSHOPID,
+    KFM_PASSWORD,
+    UPLAND_URI
 }
